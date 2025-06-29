4eae002e19dde858181cb8ec36939c88b3d67127
SuTang-vain <1945564086@qq.com>
2025-06-29 16:09:57 +0800

    feat: 初始化项目结构

--- src/main/java/com/example/badminton/admin/controller/AdminController.java
+++ src/main/java/com/example/badminton/admin/controller/AdminController.java
@@ -2,7 +2,7 @@ package com.example.badminton.admin.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
