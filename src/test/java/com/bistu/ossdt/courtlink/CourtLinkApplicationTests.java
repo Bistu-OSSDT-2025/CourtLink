299f66413c17933f60e6b2d9ce4cc1f70b59bf05
SuTang-vain <1945564086@qq.com>
2025-06-29 08:08:53 +0800

    Initial Spring Boot project setup

---
+++ src/test/java/com/bistu/ossdt/courtlink/CourtLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bistu.ossdt.courtlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
